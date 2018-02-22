f450bc6c0925ea34cff3497939f5f0f47aa11535
VladA <vladeuvlad18@gmail.com>
2018-02-22 09:52:32 +0000

    Update screen.php

--- screen.php
+++ screen.php
@@ -1,6 +1,6 @@
 <?php
 
-$myHeight = nl2br('Your Screen Width is \r' .  $_POST['width']);
+$myHeight = nl2br("Your Screen Width is \n" .  $_POST['width']);
 $myWidth = 'Your Screen Heigth is ' . $_POST['height'];
 
 
